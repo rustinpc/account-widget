6ec8956caf387b81d54a08ae6e226a4381d6eb43
bnghiem <bpnghiem@gmail.com>
2016-09-29 11:34:04 -0700

    Tossing out the trash
    back to the basics (mostly)

--- js/Landing.jsx
+++ js/Landing.jsx
@@ -1,11 +1,8 @@
 const React = require('react')
-const { Link } = require('react-router')
 
 const Landing = () => (
   <div className='home-info'>
-    <h1 className='title'>svideo</h1>
-    <input className='search' type='text' placeholder='Search' />
-    <Link to='/search' className='browse-all'> or Browse All</Link>
+    boop
   </div>
 )
 
